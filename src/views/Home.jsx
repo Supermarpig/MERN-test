700e1e59b86774eb68ee52bdfb7d9516705cd9ec
Cody <ken51717@gmail.com>
2023-07-13 15:44:21 +0800

    優化shortUrl Function

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -30,118 +30,178 @@ function Home() {
 
 
   //獲取 user輸入的Url
-  const shortUrl = () => {
+  // const shortUrl = () => {
+  //   const longUrl = user_input.current.value;
+
+  //   //檢查輸入的是不是URL
+  //   if (IsURL(longUrl)) {
+  //     //TODO:新增確認是不是有效網址
+  //     //把item清空
+  //     setItem([]);
+
+  //     //先判斷是否有相同的url---------------------------------------------------------------
+  //     getData().then((result) => {
+  //       let newArray = [];
+  //       newArray = [...result.data];
+  //       setItem(newArray);
+  //     }).catch(err => {
+  //       console.log(err);
+  //     })
+
+  //     //將全部的lonUrl丟進這個array
+  //     let urlArray = item.map(item => item.longUrl);
+  //     let hasUrl = urlArray.includes(longUrl);
+  //     //如果有一個url 
+  //     if (hasUrl) {
+  //       getDataCheck(user_input);
+  //     }
+
+  //     //如果沒東西就新增
+  //     else if (!hasUrl) {
+
+  //       if (hasUrl) {
+
+  //       } else {
+  //         //先將user 輸入的網址 新增資料庫---------------------------------------------------------------
+  //         apiUrlPost({
+  //           'title': "",
+  //           'description': "",
+  //           'imageUrl': "",
+  //           'longUrl': longUrl ? `${longUrl}` : "",
+  //           'shortUrl': ""
+  //         })
+  //           .then(res => {
+  //             // console.log("新增成功");
+  //             getData().then((result) => {
+
+  //               let newArray = [];
+  //               newArray = [...result.data];
+  //               setItem(newArray);
+  //             }).catch(err => {
+  //               console.log(err);
+  //             })
+  //             alert('新增成功')
+  //           })
+  //           .catch(error => {
+  //             // response攔截器會先執行，除非你漏接了，才會進到catch
+  //             console.log("新增失敗");
+  //           })
+
+  //         //然後傳 _ID回來---------------------------------------------------------------
+
+  //         setTimeout(() => {
+  //           getUrl(longUrl).then((result) => {
+  //             // console.log(result)
+  //             setChangeUrl(result.data._id);
+  //             let oldUrl = result.data._id
+
+  //             //將獲取的ID 弄成短網址
+  //             // 生成固定的、加密的短網址（例如，長度為 6 位）
+  //             oldUrl && generateShortUrl(oldUrl, 6)
+  //               .then(shortUrlTest => {
+  //                 // console.log(shortUrlTest);
+  //                 setShort(shortUrlTest);
+
+  //                 //新增短網址到資料庫內---------------------
+  //                 userPut(oldUrl,
+
+  //                   {
+  //                     'shortUrl': `${shortUrlTest}`
+  //                   })
+
+  //                 //一分鐘後刪除這筆資料
+  //                 setTimeout(() => {
+  //                   userDelete(oldUrl);
+  //                   alert(`刪除一筆${oldUrl}`);
+  //                   setShort('');
+  //                   // console.log(`刪除一筆${oldUrl}`)
+  //                 }, 30000)
+
+
+  //               })
+  //               .catch(error => {
+  //                 console.error(error);
+  //               });
+  //           }).catch(err => {
+  //             console.log(err);
+  //           })
+  //         }, 500);
+  //       }
+  //     }
+  //     // } else {
+  //     // console.log("URL is valid but not accessible.");
+  //     // alert('你輸入的網址 好像不能連線唷')
+  //     // }
+  //     // })
+  //     // .catch(error => {
+  //     // console.log("Error occurred while checking URL validity.");
+  //     // });
+
+  //   } else {
+  //     alert('請輸入正確的格式')
+  //   }
+  // }
+  const shortUrl = async () => {
     const longUrl = user_input.current.value;
-
-    //檢查輸入的是不是URL
-    if (IsURL(longUrl)) {
-      //TODO:新增確認是不是有效網址
-      //把item清空
-      setItem([]);
-
-      //先判斷是否有相同的url---------------------------------------------------------------
-      getData().then((result) => {
-        let newArray = [];
-        newArray = [...result.data];
-        setItem(newArray);
-      }).catch(err => {
-        console.log(err);
-      })
-
-      //將全部的lonUrl丟進這個array
-      let urlArray = item.map(item => item.longUrl);
-      let hasUrl = urlArray.includes(longUrl);
-      //如果有一個url 
+  
+    // 檢查輸入的是否為URL
+    if (!IsURL(longUrl)) {
+      alert('請輸入正確的格式');
+      return;
+    }
+  
+    try {
+      // 先判斷是否有相同的URL
+      const result = await getData();
+      const newArray = [...result.data];
+      setItem(newArray);
+  
+      // 檢查是否存在相同的URL
+      const hasUrl = newArray.some(item => item.longUrl === longUrl);
+  
       if (hasUrl) {
         getDataCheck(user_input);
-
+      } else {
+        // 新增資料到資料庫
+        const postData = {
+          title: "",
+          description: "",
+          imageUrl: "",
+          longUrl: longUrl,
+          shortUrl: ""
+        };
+        const res = await apiUrlPost(postData);
+  
+        // 更新資料
+        const updatedData = await getData();
+        setItem(updatedData.data);
+        alert('新增成功');
+  
+        // 取得短網址
+        const result = await getUrl(longUrl);
+        setChangeUrl(result.data._id);
+        const oldUrl = result.data._id;
+  
+        // 生成固定的、加密的短網址（例如，長度為 6 位）
+        const shortUrlTest = await generateShortUrl(oldUrl, 6);
+        setShort(shortUrlTest);
+  
+        // 更新短網址到資料庫
+        await userPut(oldUrl, { shortUrl: shortUrlTest });
+  
+        // 一分鐘後刪除這筆資料
+        setTimeout(async () => {
+          await userDelete(oldUrl);
+          alert(`刪除一筆${oldUrl}`);
+          setShort('');
+        }, 30000);
       }
-
-      //如果沒東西就新增
-      else if (!hasUrl) {
-
-        if (hasUrl) {
-
-        } else {
-          //先將user 輸入的網址 新增資料庫---------------------------------------------------------------
-          apiUrlPost({
-            'title': "",
-            'description': "",
-            'imageUrl': "",
-            'longUrl': longUrl ? `${longUrl}` : "",
-            'shortUrl': ""
-          })
-            .then(res => {
-              // console.log("新增成功");
-              getData().then((result) => {
-
-                let newArray = [];
-                newArray = [...result.data];
-                setItem(newArray);
-              }).catch(err => {
-                console.log(err);
-              })
-              alert('新增成功')
-            })
-            .catch(error => {
-              // response攔截器會先執行，除非你漏接了，才會進到catch
-              console.log("新增失敗");
-            })
-
-          //然後傳 _ID回來---------------------------------------------------------------
-
-          setTimeout(() => {
-            getUrl(longUrl).then((result) => {
-              // console.log(result)
-              setChangeUrl(result.data._id);
-              let oldUrl = result.data._id
-
-              //將獲取的ID 弄成短網址
-              // 生成固定的、加密的短網址（例如，長度為 6 位）
-              oldUrl && generateShortUrl(oldUrl, 6)
-                .then(shortUrlTest => {
-                  // console.log(shortUrlTest);
-                  setShort(shortUrlTest);
-
-                  //新增短網址到資料庫內---------------------
-                  userPut(oldUrl,
-
-                    {
-                      'shortUrl': `${shortUrlTest}`
-                    })
-
-                  //一分鐘後刪除這筆資料
-                  setTimeout(() => {
-                    userDelete(oldUrl);
-                    alert(`刪除一筆${oldUrl}`);
-                    setShort('');
-                    // console.log(`刪除一筆${oldUrl}`)
-                  }, 30000)
-
-
-                })
-                .catch(error => {
-                  console.error(error);
-                });
-            }).catch(err => {
-              console.log(err);
-            })
-          }, 500);
-        }
-      }
-      // } else {
-      // console.log("URL is valid but not accessible.");
-      // alert('你輸入的網址 好像不能連線唷')
-      // }
-      // })
-      // .catch(error => {
-      // console.log("Error occurred while checking URL validity.");
-      // });
-
-    } else {
-      alert('請輸入正確的格式')
+    } catch (error) {
+      console.error(error);
+      alert('操作失敗');
     }
-  }
+  };
+  
   // 確認Url 是否存在
   function getDataCheck(user_input) {
 
